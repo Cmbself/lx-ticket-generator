b99b199afcd0b7d4982a0c5c131cff09208fb0ab
Cmbself <88185930+Cmbself@users.noreply.github.com>
2025-08-01 00:42:49 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'https://image.fosky.top/projects/ticket/' : './',
+  base: process.env.NODE_ENV === 'production' ? './' : './',
   plugins: [
     vue(),
   ],
